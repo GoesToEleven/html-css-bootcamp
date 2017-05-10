42ae56a69727060c96aa3226765782f8a060fd2f
Todd McLeod <tuddleymc@gmail.com>
2017-05-09 22:42:45 -0700

    you're doing great

---
+++ 000_temp/09_SPRING-2017/032-go-form/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", process)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	fn := r.FormValue("gender")
+	tpl.ExecuteTemplate(w ,"process.gohtml", fn)
+}
